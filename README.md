57602b93e1bc47d79765b29c27cd9004fbad62c7
Alyssa Diaz <email": null>
2015-09-29 10:41:40 -0500

    Adds routes and listed stories in readme

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 ## Hacker News Clone Reference
 
 Just a hacker news clone reference rails app
+
+Requirements:
+
+- An index page listing all the posts
+- A page showing a particular post, including comments
+- A user profile page
+- A page showing all a user's submissions
+- A page showing all a user's comments
+- A user signup page (2 routes)
+- A login page (2 routes)
+- A way to logout
+- A user should be able to create a post (2 routes)
+- A user should be able to comment on a post (2 routes)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  resources :users, only: [:new, :create, :show]  do
+    resources :posts, only: [:index]
+    resources :comments, only: [:index]
+  end
+
+  resources :posts, only: [:index, :show, :new, :create] do
+    resources :comments, only: [:new, :create]
+  end
+
+  root 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -11,7 +24,7 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
+  # Example resource route (maps HTTP verbs to crailstroller actions automatically):
   #   resources :products
 
   # Example resource route with options:
